8c5951810b839861acd3592cb3553eb182932ee0
Thiago.Martins <tthome3@hotmail.com>
2021-01-19 18:23:02 +0100

    commit main updated1

--- main.py
+++ main.py
@@ -1,22 +1,73 @@
+import collections
+import dash
+import pandas as pd
+import numpy as np
+
 import base64
-import datetime
+import datetime as dt
 import io
 
-import dash
-from dash.dependencies import Input, Output, State
-import dash_core_components as dcc
+from dash.dependencies import Output, Input, State
+from dash.exceptions import PreventUpdate
+
 import dash_html_components as html
+import dash_core_components as dcc
 import dash_table
 
-import pandas as pd
+import plotly.express as px
+
+app = dash.Dash(__name__)
 
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+days = {1, 2, 3, 4, 5, 6, 7, 14, 21}
+used_acriss = {'MBMR', 'EDMR', 'CDMR', 'IDMD', 'CCAR', 'PDMD', 'JVMD', 'SVMD'}
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+bench_columns = {'Fecha': 'Date', 'Site': 'Web', 'Días': 'Days',
+         'Coche': 'Car', 'Categoria': 'Category',
+         'Acriss': 'Acriss', 'Transmisión': 'Transmition',
+         'Asientos': 'Seats', 'Puertas': 'Doors',
+         'Proveedor': 'Competitor', 'Precio': 'Price'}
+
+
+def pivot_table_bench(df, value_acriss='MBMR', value_days=7):
+    df.rename(columns=bench_columns, inplace=True)
+    df.dropna(inplace=True, axis=0)
+    pricing_pivot = pd.pivot_table(df,
+                                   index=['Competitor', 'Days', 'Date', 'Acriss'],
+                                   values=['Price'],
+                                   aggfunc=np.min)
+    pricing_pivot.reset_index(inplace=True)
+    pricing_pivot['Date'] = pd.to_datetime(pricing_pivot['Date']).dt.strftime("%d-%m-%Y").astype('datetime64[ns]')
+    pricing_pivot = pricing_pivot[(pricing_pivot.Acriss == value_acriss) & (pricing_pivot.Days == value_days)]
+    return pricing_pivot
+
+def parse_contents(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xlsx' in filename:
+            # Assume that the user uploaded an excel file
+            sh_bench = pd.read_excel(io.BytesIO(decoded), sheet_name='bench')
+            df = pivot_table_bench(sh_bench)
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    return df.to_json(date_format='iso', orient='split')
+
+def create_figure(dff):
+    fig = px.line(dff,
+        x="Date", y="Price", color='Competitor')
+    return fig
 
 app.layout = html.Div([
+    dcc.Store(id='memory-output',
+              storage_type='session'),
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -36,59 +87,37 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
-    html.Div(id='output-data-upload'),
+    # Hidden div inside the app that stores the intermediate value
+    html.Div(id='intermediate-value', style={'display': 'none'}),
+
+    dcc.Dropdown(id='memory-days', options=[
+        {'value': x, 'label': x} for x in days
+    ], multi=True, value=[7]),
+    dcc.Dropdown(id='memory-acriss', options=[
+        {'value': x, 'label': x} for x in used_acriss
+    ], multi=True, value=['MBMR']),
+    dcc.Graph(id='graph'),
+
 ])
-
-
-def parse_contents(contents, filename, date):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xlsx' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return html.Div([
-        html.H5(filename),
-        html.H6(datetime.datetime.fromtimestamp(date)),
-
-        dash_table.DataTable(
-            data=df.to_dict('records'),
-            columns=[{'name': i, 'id': i} for i in df.columns]
-        ),
-
-        html.Hr(),  # horizontal line
-
-        # For debugging, display the raw contents provided by the web browser
-        html.Div('Raw Content'),
-        html.Pre(contents[0:200] + '...', style={
-            'whiteSpace': 'pre-wrap',
-            'wordBreak': 'break-all'
-        })
-    ])
-
-
-@app.callback(Output('output-data-upload', 'children'),
+# Load data in-memory: type session
+@app.callback(Output('memory-output', 'data'),
               Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
+              State('upload-data', 'filename'))
+def update_output(list_of_contents, list_of_names):
     if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+        for c, n in zip(list_of_contents, list_of_names):
+            children = parse_contents(c, n)
         return children
 
+@app.callback(Output('graph', 'figure'), Input('memory-output', 'data'))
+def update_graph(jsonified_cleaned_data):
+    if jsonified_cleaned_data is None:
+        raise PreventUpdate
+    else:
+        dff = pd.read_json(jsonified_cleaned_data, orient='split')
+
+        figure = create_figure(dff)
+        return figure
 
 
 if __name__ == '__main__':
